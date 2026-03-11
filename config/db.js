a6d949ec7b70560d65f943057edc0bc82a9f303a
shirishashiriii2005-maker <Shirishashirii2005@gmail.com>
2026-03-11 20:19:54 +0530

    first commit

--- config/db.js
+++ config/db.js
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
+
 const dbConnect = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected");
   } catch (err) {
-    console.log(err);
+    console.log("MongoDB connection failed:", err);
   }
 };
+
 export default dbConnect;
